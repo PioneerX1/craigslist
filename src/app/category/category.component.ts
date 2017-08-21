753a4896ad0e31c6c34cdf2b8b96ba597426da39
Mick Sexton <lacrookedbeat@hotmail.com>
2017-08-21 16:11:15 -0700

    add titles of various listings on the home page, next step is to break out categories

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Listing } from '../listing.model';
+import { ListingService } from '../listing.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
+  providers: [ListingService]
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private listingService: ListingService) { }
 
   ngOnInit() {
+    this.listings = this.listingService.getListings();
   }
+
+  listings: Listing[];
 
 }
---
+++ src/app/mock-categories.ts
@@ -0,0 +1,8 @@
+import { Category } from './category.model';
+
+export const CATEGORIES: Category[] = [
+  new Category("For Sale", 1),
+  new Category("Let's Jam", 2),
+  new Category("Here's some Jobs", 3),
+  new Category("Weird people meeting on the internet", 4)
+];
